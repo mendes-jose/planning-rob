d4447ad2ca9c554a1fc92b3c689fbf3191033da3
José Magno MENDES FILHO <josemagno.mendes@gmail.com>
2015-04-08 09:30:48 +0200

    Cleaning up

--- src/tests-pyOpt/launch-test2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-python planning-sim-altth.py > /home/mendes/Dropbox/planning-test/planning-sim-altth-output.txt
-#python planning-sim-dl.py >    /home/mendes/Dropbox/planning-test/planning-sim-dl-output.txt
-#python planning-sim-sdl.py >   /home/mendes/Dropbox/planning-test/planning-sim-sdl-output.txt
-python planning-sim-svl.py >   /home/mendes/Dropbox/planning-test/planning-sim-svl-output.txt
-#python planning-sim-th.py >    /home/mendes/Dropbox/planning-test/planning-sim-th-output.txt
-python planning-sim-vl.py >    /home/mendes/Dropbox/planning-test/planning-sim-vl-output.txt
